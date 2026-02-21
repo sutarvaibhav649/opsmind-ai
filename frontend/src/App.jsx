4f411524503bb3b6312976ed70efbb2b80510c76
SRD-web <sairupajupalle@gmail.com>
2026-02-21 18:34:47 +0530

    Refactor App component to include Navbar and MainLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Navbar from "./Navbar";
+import MainLayout from "./MainLayout";
 
-const App = () => {
-    return (
-        <div className=' text-red-400'>Hello</div>
-    )
+function App() {
+  return (
+    <div className="h-screen bg-gray-900 text-white flex flex-col">
+      <Navbar />
+      <MainLayout />
+    </div>
+  );
 }
 
-export default App
+export default App;
